6601d736801ab0ca9cd107a803007aaf5fe95194
NicolaTeora <nikxodia@gmail.com>
2024-01-24 17:26:25 +0100

    feat : I turn VIPs into objects

--- JS/function.js
+++ JS/function.js
@@ -1,1 +1,11 @@
-console.log('online-2')
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function placeVip(name, indice){
+    return {
+        Tavolo: 'Tavolo Vip',
+        nome_Vip: name,
+        posto: indice,
+    };
+}
--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,30 @@
-console.log('online')
+//snak 1
+const vips = [
+  "Dwayne Johnson",
+  "Brad Pitt",
+  "Johnny Depp",
+  "Lady Gaga",
+  "Cristiano Ronaldo",
+  "Georgina Rodriguez",
+  "Chiara Ferragni",
+  "Fedez",
+  "George Clooney",
+  "Amal Clooney",
+  "Maneskin",
+];
+let nameVipGuest;
+let tableVips = [];
+
+console.log(vips);
+
+//scorro gli elementi dell'array vips per indicizzarli
+for (listVip of vips) {
+  nameVipGuest = listVip;
+  console.log(placeVip(nameVipGuest));
+}
+
+//riformulo l'array con gli elementi modificati in object
+tableVips = vips.map((elemento, index) => {
+  return placeVip(elemento, index);
+});
+console.log(tableVips);
