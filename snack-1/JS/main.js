68c737e5f8d9e6137c6e0782bf0d0a9b0d75759b
NicolaTeora <nikxodia@gmail.com>
2024-01-24 17:33:24 +0100

    fix : folder

--- snack-1/JS/main.js
+++ snack-1/JS/main.js
@@ -1,4 +1,5 @@
 //snak 1
+/*
 const vips = [
   "Dwayne Johnson",
   "Brad Pitt",
@@ -28,3 +29,44 @@ tableVips = vips.map((elemento, index) => {
   return placeVip(elemento, index);
 });
 console.log(tableVips);
+*/
+
+const students = [
+  {
+    id: 213,
+    name: "Marco della Rovere",
+    grades: 78
+  },
+  {
+    id: 110,
+    name: "Paola Cortellessa",
+    grades: 96
+  },
+  {
+    id: 250,
+    name: "Andrea Mantegna",
+    grades: 48
+  },
+  {
+    id: 145,
+    name: "Gaia Borromini",
+    grades: 74
+  },
+  {
+    id: 196,
+    name: "Luigi Grimaldello",
+    grades: 68
+  },
+  {
+    id: 102,
+    name: "Piero della Francesca",
+    grades: 50
+  },
+  {
+    id: 120,
+    name: "Francesca da Polenta",
+    grades: 84
+  }
+];
+
+console.log(students);
