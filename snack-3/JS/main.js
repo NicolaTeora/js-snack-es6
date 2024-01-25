3adc0b8e548f4c91a7c084309bfb81e2dbdc3e97
NicolaTeora <nikxodia@gmail.com>
2024-01-25 18:57:36 +0100

    feat : test decostructuring array

--- snack-3/JS/main.js
+++ snack-3/JS/main.js
@@ -1,1 +1,41 @@
-console.log('online')
+/*
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà:
+nome e peso.
+*/
+const bicycles = [
+    {
+      name: 'Mountain Bike',
+      peso: 50,
+    },
+    {
+      name: 'Superleggera',
+      peso: 35,
+    },
+    {
+      name: 'Pinarello Dogma',
+      peso: 44,
+    },
+    {
+      name: 'Trek Madone',
+      peso: 60, 
+    },
+    {
+      name: 'Giant Propel Advanced',
+      peso: 25,      
+    },
+];
+let weightBike = [];
+//Stampare in console la bici con peso minore utilizzando destructuring e template literal.
+
+console.table(bicycles);
+
+let [bike1, bike2, bike3, bike5, bike6] = bicycles;
+//console.log(bike1, bike2, bike3, bike5, bike6);
+
+for (let index = 0; index < bicycles.length; index++) {
+    let bike = bicycles[index].peso;
+    console.log(bike);
+    weightBike.push(bike);
+}
+console.log(weightBike)
